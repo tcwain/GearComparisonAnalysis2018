a0204023af38bd7dd95b4269116b040ffe7c9fff
Tom Wainwright <tcwainw@gmail.com>
2018-09-13 21:18:02 -0700

    Corrected data selection, built plots

--- inst/scripts/SizeSelectivity.R
+++ inst/scripts/SizeSelectivity.R
@@ -16,7 +16,9 @@ lenData <- MMEDdata[ , c("Cruise","MMED","Species","Length",
                          "Number","Distance","Haul")]
 lenData <- lenData[lenData$Species %in% len.spec, ]
 lenData <- lenData[!is.na(lenData$Length), ]
-lenData$Len5mm <- 5 * round(lenData$Length/5)
+# Length bin size (mm)
+binsize <- 5
+lenData$LenBin <- binsize * round(lenData$Length/binsize)
 print(summary(lenData)) ### NOT RUN
 
 ## ---- SizeSummary
@@ -32,7 +34,7 @@ for (excl in c("Up","Down")) {
   .tab <- with(lD, tapply(Number, list(Species), sum, na.rm=T))
   print(.tab)
   lf.sel.spec <- names(.tab)[.tab >= 100]
-  lenFreq <- with(lD, tapply(Number, list(Len5mm, MMED, Species), sum))
+  lenFreq <- with(lD, tapply(Number, list(LenBin, MMED, Species), sum))
   .mfrow <- if(lndscp) c(3,3) else c(3,2)
   par(mfrow=.mfrow, omi=c(0.5,0.5,0,0), mar=c(3,3,2,1))
   for (sp in lf.sel.spec) {
@@ -41,8 +43,8 @@ for (excl in c("Up","Down")) {
       .dat <- lenFreq[ , , sp]
       .maxN <- max(.dat,na.rm=T)
       .len <- lD[lD$Species %in% sp, ]
-      .maxL <- max(.len$Length, na.rm=T)
-      .minL <- min(.len$Length, na.rm=T)
+      .maxL <- max(.len$LenBin, na.rm=T)
+      .minL <- min(.len$LenBin, na.rm=T)
       plot(as.numeric(rownames(.dat)), .dat[, 'None'], type='h', col=BLACK, lwd=1,
            xlim=c(.minL,.maxL), ylim=c(-.maxN,.maxN), axes=F,
            xlab='', ylab='')
@@ -83,9 +85,9 @@ klwg_GLMM <- function(sfdat) {
 }
 
 klwg_SCMM <- function(sfdat) {
-  NumTotL <- with(sfdat, tapply(Number, list(Length, MMED), sum, na.rm=TRUE))
+  NumTotL <- with(sfdat, tapply(Number, list(LenBin, MMED), sum, na.rm=TRUE))
 ##  print(dim(NumTotL))   ### DEBUG ###
-  EffTotL <- with(sfdat, tapply(Distance, list(Length, MMED),sum, na.rm=TRUE))
+  EffTotL <- with(sfdat, tapply(Distance, list(LenBin, MMED),sum, na.rm=TRUE))
 ##  print(dim(EffTotL))   ### DEBUG ###
   cpue <- NumTotL/EffTotL
   cpue[is.na(cpue)] <- 0
@@ -120,29 +122,59 @@ for (excl in c("Up","Down")) {
   .tab <- with(lD, tapply(Number, list(Species), sum, na.rm=T))
   print(.tab)
   lf.sel.spec <- names(.tab)[.tab >= 100]
-  lenFreq <- with(lD, tapply(Number, list(Len5mm, MMED, Species), sum, na.rm=TRUE))
+  lenFreq <- with(lD, tapply(Number, list(LenBin, MMED, Species), sum, na.rm=TRUE))
   .mfrow <- if(lndscp) c(3,3) else c(3,2)
-  par(mfrow=.mfrow, omi=c(0.5,0.5,0,0), mar=c(3,3,2,1))
-  ## for(sp in c("COHO SALMON")) {       ### TESTING ###
+  par(mfrow=.mfrow, omi=c(0.5,0.5,0,0.5), mar=c(3,3,2,3))
+  ##for(sp in c("COHO SALMON")) {       ### TESTING ###
   for (sp in lf.sel.spec) {
     cat('****************', sp, '*****************\n')
     if (sp %in% dimnames(lenFreq)[[3]]) {
       .dat <- lenFreq[ , , sp]
+      .maxN <- max(.dat,na.rm=T)
+      ##print(.dat)     ### DEBUG ###
       .len <- lD[lD$Species %in% sp, ]
+      .maxL <- max(.len$Length, na.rm=T)
+      .minL <- min(.len$Length, na.rm=T)
       .len.std <- .len[.len$MMED=='None', ]
       .len.mmed <- .len[.len$MMED==excl, ]
-      .x <- rep(.len.std$Length, .len.std$Number)
-      .y <- rep(.len.mmed$Length, .len.mmed$Number)
+      .x <- rep(.len.std$LenBin, .len.std$Number)
+      .y <- rep(.len.mmed$LenBin, .len.mmed$Number)
       if ((length(.x) > 10) & (length(.y) > 10)) {
         glmm <- klwg_GLMM(sfdat=.len)
         scmm <- klwg_SCMM(sfdat=.len)
-        pred <- predict(scmm, type="response", se.fit=TRUE)
-        plot(scmm$model$L, scmm$model$p.L12, col="blue",
-                        main=paste(sp, ":", excl))
-        abline(h=0.5, col="red")
-        lines(scmm$model$L, pred$fit)
-        lines(scmm$model$L, pred$fit + 2*pred$se.fit, lty=2)
-        lines(scmm$model$L, pred$fit - 2*pred$se.fit, lty=2)
+        print(summary(scmm))
+        L.pred <- seq(.minL, .maxL, binsize)
+        pred <- predict(scmm, newdata=data.frame(L=L.pred, wts=1.0),
+                        type="response", se.fit=TRUE)
+        p.pred <- pred$fit
+        p.UL <- p.pred + 1.96*pred$se.fit
+        p.LL <- p.pred - 1.96*pred$se.fit
+        CR.obs <- 1/scmm$model$p.L12 - 1
+        CR.pred <- 1/p.pred - 1
+        CR.UL <- 1/p.LL - 1
+        CR.UL[CR.UL > 1000] <- 1000 #recode infinite values
+        CR.LL <- 1/p.UL - 1
+        CR.LL[CR.LL < 0.001] <- 0.001 # recode zeros for log plot
+        plot(as.numeric(rownames(.dat)), .dat[, 'None'], type='h', col=BLACK, lwd=1,
+             xlim=c(.minL,.maxL), ylim=c(-.maxN,.maxN), axes=F,
+             xlab='', ylab='')
+        box()
+        axis(side=1, lwd=0, lwd.ticks=1)
+        axis(side=2, at=pretty(c(-max(.dat,na.rm=T),max(.dat,na.rm=T))),
+             labels=abs(pretty(c(-max(.dat,na.rm=T),max(.dat,na.rm=T)))))
+        points(as.numeric(rownames(.dat)), -.dat[, excl], type='h',
+               col={if(excl %in% "Up") RED else BLUE}, lwd=1)
+        mtext(sp, side=3, cex=0.75, line=0)
+        abline(h=0, lwd=1)
+        # add Catch Ratio plot on right axis
+        par(new=TRUE)
+        plot(L.pred, CR.pred, log="y", type='l', lwd=2, axes=FALSE, bty="n",
+             ylim=c(1/50, 50), xlab="", ylab="")
+        axis(side=4, at=c(0.02, 0.2, 1, 5, 50),
+             labels=c("0.02", "0.2", "1", "5", "50"))
+        # abline(h=1.0, col="red")
+        lines(L.pred, CR.UL, lty=2, lwd=2)
+        lines(L.pred, CR.LL, lty=2, lwd=2)
         beta <- klwg_BetaR(sfdat=.len)
       } else {
         cat('\n Insufficient data \n')
@@ -151,6 +183,9 @@ for (excl in c("Up","Down")) {
       cat('\n\tNO LENGTH DATA FOR ', sp, '\n')
     } # if (sp %in% ...)
   } # for(sp)
+  mtext('Size (mm)', side=1, outer=TRUE, at=c(0.5,0.5), cex=1.5)
+  mtext('Observed Catch per km', side=2, outer=TRUE, at=c(0.5,0.5), cex=1.5)
+  mtext('Estimated Catch Ratio', side=4, outer=TRUE, at=c(0.5,0.5), cex=1.5)
 } # for (excl)
 
 # Implementation of "double-bootstrap"
