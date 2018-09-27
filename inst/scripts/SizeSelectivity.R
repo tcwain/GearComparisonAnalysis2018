2174d013c9993d14824e31ba596cee8da87ff228
Tom Wainwright <tcwainw@gmail.com>
2018-09-27 11:55:11 -0700

    Added back Wilcox & KS tests, cleaning up code.

--- inst/scripts/SizeSelectivity.R
+++ inst/scripts/SizeSelectivity.R
@@ -19,15 +19,12 @@ lenData <- MMEDdata[ , c("Cruise","MMED","Species","Length",
 cnt <- with (lenData, tapply(Number, list(Haul, Species), FUN=sum, simplify=T))
 cnt[is.na(cnt)] <- 0
 cnt <- as.data.frame(cnt)
-## print(summary(cnt)) ### DEBUG ###
 # Total measured by Haul & species
 meas <- with(lenData, tapply(!is.na(Length), list(Haul, Species), FUN=sum, simplify=T))
 meas[is.na(meas)] <- 0
 meas <- as.data.frame(meas)
-## print(summary(meas)) ### DEBUG ###
 # Subsampling ratio by Haul & Species:
 ssr <- meas / cnt
-##print(summary(ssr)) ### DEBUG ###
 # Adjusted Numbers (expanded by ssr)
 lenData <- lenData[!is.na(lenData$Length), ] #remove non-measured counts
 lenData$AdjNum <- lenData$Number / unlist(apply(lenData[c("Haul","Species")], 1,
@@ -37,64 +34,8 @@ lenData <- lenData[!is.na(lenData$Length), ] #Already accounted for in AdjNum
 # Length bin size (mm)
 binsize <- 5
 lenData$LenBin <- binsize * round(lenData$Length/binsize)
-print(summary(lenData)) ### DEBUG ###
 
-## ---- SizeSummary
-
-for (excl in c("Up","Down")) {
-  cat('**************** Excluder: ', excl, ' *****************\n')
-  lD <- if(excl %in% "Up") {
-    lenData[lenData$Cruise %in% c(41,43,50), ]
-  } else {
-    lenData[lenData$Cruise %in% 53, ]
-  }
-  lD$Species <- factor(as.character(lD$Species))
-  .tab <- with(lD, tapply(Number, list(Species), sum, na.rm=T))
-  print(.tab)
-  lf.sel.spec <- names(.tab)[.tab >= 100]
-  lenFreq <- with(lD, tapply(Number, list(LenBin, MMED, Species), sum))
-  .mfrow <- if(lndscp) c(3,3) else c(3,2)
-  par(mfrow=.mfrow, omi=c(0.5,0.5,0,0), mar=c(3,3,2,1))
-  for (sp in lf.sel.spec) {
-    cat('****************', sp, '*****************\n')
-    if (sp %in% dimnames(lenFreq)[[3]]) {
-      .dat <- lenFreq[ , , sp]
-      .maxN <- max(.dat,na.rm=T)
-      .len <- lD[lD$Species %in% sp, ]
-      .maxL <- max(.len$LenBin, na.rm=T)
-      .minL <- min(.len$LenBin, na.rm=T)
-      plot(as.numeric(rownames(.dat)), .dat[, 'None'], type='h', col=BLACK, lwd=1,
-           xlim=c(.minL,.maxL), ylim=c(-.maxN,.maxN), axes=F,
-           xlab='', ylab='')
-      axis(side=1, lwd=0, lwd.ticks=1)
-      axis(side=2, at=pretty(c(-max(.dat,na.rm=T),max(.dat,na.rm=T))),
-           labels=abs(pretty(c(-max(.dat,na.rm=T),max(.dat,na.rm=T)))))
-      points(as.numeric(rownames(.dat)), -.dat[, excl], type='h',
-             col={if(excl %in% "Up") RED else BLUE}, lwd=1)
-      mtext(sp, side=3, cex=0.75, line=0)
-      abline(h=0, lwd=1)
-      .len.std <- .len[.len$MMED=='None', ]
-      .len.mmed <- .len[.len$MMED==excl, ]
-      .x <- rep(.len.std$Length, .len.std$Number)
-      .y <- rep(.len.mmed$Length, .len.mmed$Number)
-      boxplot(list(.x,.y), add=TRUE, horizontal=TRUE, boxwex=0.2*.maxN,
-              at=0.25*c(.maxN,-.maxN), lwd=2, col=rgb(1,1,1,0.5),
-              outline=FALSE, axes=FALSE)
-      if ((length(.x) > 10) & (length(.y) > 10)) {
-        print(wilcox.test(.x, .y, alt="two.sided"))
-        print(ks.test(.x, .y))
-      } else {
-        cat('\n Insufficient data for KS test \n')
-      } # if (length...)
-    } else {
-      cat('\n\tNO LENGTH DATA FOR ', sp, '\n')
-    } # if (sp %in% ...)
-  } # for(sp)
-  mtext('Size (mm)', side=1, outer=TRUE, cex=1.5)
-  mtext('Number Caught', side=2, outer=TRUE, cex=1.5)
-} # for (excl)
-
-## ---- SizeSelMethods
+## ---- SizeSelAnal
 
 boot_SCMM <- function(sdat, nrep=10, binsz=5, L.pr=NULL) {
   fit.model <- function(sdat) {
@@ -104,19 +45,14 @@ boot_SCMM <- function(sdat, nrep=10, binsz=5, L.pr=NULL) {
                                  na.rm=TRUE, default=0))
     cpue <- NumTotL/EffTotL
     cpue[is.na(cpue)] <- 0
-    ##print(summary(cpue))  ### DEBUG ###
     std <- cpue[ , 1]
     tst <- cpue[ , 2]
     p.L12 <- std / (std + tst)
-    ##print(summary(p.L12))   ### DEBUG ###
     # Binomial weights based on number measured in both gears:
     Nmeas <- with(sdat, tapply(Number, list(LenBin, MMED), sum,
                                na.rm=TRUE, default=0))
-    ##print(Nmeas)  ### DEBUG ###
     wts <- Nmeas[ , 1] + Nmeas[ , 2]
-    ##print(summary(wts))  ### DEBUG ###
     L <- as.numeric(names(p.L12))
-    ##print(summary(L))   ### DEBUG ###
     old.opt <- options(warn = -1) # suppress warnings about non-integer values
     fit.gam <- mgcv::gam(p.L12 ~ s(L, bs="cr", k=5), family=binomial, weights=wts)
     options(old.opt)
@@ -127,23 +63,18 @@ boot_SCMM <- function(sdat, nrep=10, binsz=5, L.pr=NULL) {
   fit.full <- fit.model(sdat)
   # Predictions of full model, with rough SE's
   if (is.null(L.pr)) L.pr <- seq(min(sdat$LenBin), max(sdat$LenBin), 5)
-  ##cat("L.pr: ", L.pr, "\n")   ### DEBUG ###
   pred.full <- predict(fit.full, newdata=data.frame(L=L.pr, wts=1.0),
-                  type="response")
+                       type="response")
   names(pred.full) <- L.pr
   # Bootstrap predictions:
   bs <- matrix(NA, nrow=length(L.pr), ncol=nrep, dimnames=list(L.pr, NULL))
   rep <- 0
   while (rep < nrep) {
     hauls <- unique(sdat$Haul)
-    ##print (hauls)  ### DEBUG ###
     hauls.samp <- sample(hauls, length(hauls), replace=TRUE)
-    ##print(hauls.samp)  ### DEBUG ###
     .data <- data.frame()
     for (h in hauls.samp) {
       .hdata <- sdat[sdat$Haul == h, ]
-      # TODO: resample lengths within haul
-      ##print(summary(.hdata))  ### DEBUG ###
       ssr <- with(.hdata, sum(Number)/sum(AdjNum)) # subsample rate
       L.ex <- with(.hdata, rep(LenBin, Number))  #expand Number to indiv. lengths
       if(length(L.ex) > 1) {
@@ -152,28 +83,21 @@ boot_SCMM <- function(sdat, nrep=10, binsz=5, L.pr=NULL) {
         L.smp <- L.ex  # sample() doesn't work for length 1 vector
       }
       new.freq <- as.data.frame(table(L.smp))
-      ##print(new.freq)  ### DEBUG ###
       .ndata <- data.frame(MMED=unique(.hdata$MMED),
                            Haul=unique(.hdata$Haul),
                            Distance=mean(.hdata$Distance),
                            LenBin=as.numeric(levels(new.freq$L.smp)),
                            Number=new.freq$Freq,
                            AdjNum=new.freq$Freq/ssr)
-      ##print(summary(.ndata))  ### DEBUG ###
       .data <- rbind(.data, .ndata)
     } # for (h)
     names(.data) <- names(sdat)
-    ##print(summary(.data))   ### DEBUG ###
-    ##cat("N with no MMED: ", with(.data, sum(Number[MMED=="None"])), "\n") ### DEBUG ###
-    ##cat("N with MMED: ", with(.data, sum(Number[MMED!="None"])), "\n") ### DEBUG ###
     fit.rep <- fit.model(.data)
     rep <- rep+1
-    ##print(summary(fit.rep))   ### DEBUG ###
     bs[ , rep] <- predict(fit.rep, newdata=data.frame(L=L.pr, wts=1.0),
                           type="response")
   } # for (rep)
   rownames(bs) <- L.pr
-  ##print(bs[ , 1:5])   ### DEBUG ###
   bs.mn <- apply(bs, 1, mean, na.rm=FALSE)
   bs.q <- t(apply(bs, 1, quantile, probs=c(0,0.05,0.25,0.50,0.75,0.95,1),
                   na.rm=FALSE))
@@ -181,9 +105,8 @@ boot_SCMM <- function(sdat, nrep=10, binsz=5, L.pr=NULL) {
               boot.sum=data.frame(mean=bs.mn, q=bs.q)))
 } # boot_SCMM()
 
-## ---- SizeSelAnal
 for (excl in c("Up","Down")) {
-  cat('**************** Excluder: ', excl, ' *****************\n')
+  cat('\n**************** Excluder: ', excl, ' *****************\n')
   lD <- if(excl %in% "Up") {
     lenData[lenData$Cruise %in% c(41,43,50), ]
   } else {
@@ -191,33 +114,32 @@ for (excl in c("Up","Down")) {
   }
   lD$Species <- factor(as.character(lD$Species))
   .tab <- with(lD, tapply(AdjNum, list(Species), sum, na.rm=T))
+  cat('Total Adjusted Catch:\n')
   print(.tab)
   lf.sel.spec <- names(.tab)[.tab >= 100]
   lenFreq <- with(lD, tapply(AdjNum, list(LenBin, MMED, Species), sum, na.rm=TRUE))
   lenFreq[is.na(lenFreq)] <- 0
-  ##print(summary(lenFreq))    ### DEBUG ###
   .mfrow <- if(lndscp) c(3,3) else c(3,2)
   par(mfrow=.mfrow, omi=c(0.5,0.5,0,0.5), mar=c(3,3,2,3))
-  ##for(sp in c("COHO SALMON")) {       ### TESTING ###
   for (sp in lf.sel.spec) {
-    cat('****************', sp, '*****************\n')
+    cat('\n****************', sp, '*****************\n')
     if (sp %in% dimnames(lenFreq)[[3]]) {
       .dat <- lenFreq[ , , sp]
       .maxN <- max(.dat,na.rm=T)
-      ##print(.dat)     ### DEBUG ###
       .len <- lD[lD$Species %in% sp,
                  c("MMED", "Haul", "Distance", "LenBin", "Number", "AdjNum")]
       .maxL <- max(.len$LenBin, na.rm=T)
       .minL <- min(.len$LenBin, na.rm=T)
       .len.std <- .len[.len$MMED=='None', ]
-      ##print(.len.std)     ### DEBUG ###
       .len.mmed <- .len[.len$MMED==excl, ]
-      .x <- sum(.len.std$Number)
-      ##print(.x)     ### DEBUG ###
-      .y <- sum(.len.mmed$Number)
-      ##print(.y)     ### DEBUG ###
-      if ((.x > 40) & (.y > 40)) {
-      ##if ((length(!is.na(.x)) > 40) & (length(!is.na(.y)) > 40)) {
+      .x <- rep(.len.std$LenBin, .len.std$Number)
+      .y <- rep(.len.mmed$LenBin, .len.mmed$Number)
+      # Run analysis only if > 40 measurements in each gear:
+      if ((length(.x) > 40) & (length(.y) > 40)) {
+        # Wilcox & KS test for overall difference in size-frequencies
+        print(wilcox.test(.x, .y, alt="two.sided"))
+        print(ks.test(.x, .y))
+        # SCMM method to fit Catch ratio to size:
         scmm <- boot_SCMM(sdat=.len, nrep=1000, binsz=binsize)
         cat("\tSummary of GAM fit: \n")
         print(summary(scmm$gam))
@@ -252,14 +174,6 @@ for (excl in c("Up","Down")) {
         lines(L.pred, CR.boot[ , "q.50."], lty=2, lwd=2) # bs median
         lines(L.pred, CR.boot[ , "q.5."], lty=3, lwd=2) # bs lower 5%
         lines(L.pred, CR.boot[ , "q.95."], lty=3, lwd=2) # bs upper 5%
-        ### DEBUG: ###
-        # print(scmm$gam$model)
-        # plot(scmm$gam$model$L, scmm$gam$model$p.L12, col="blue", ylim=c(-0.1,1.1))
-        # print(p.pred)
-        # lines(L.pred, p.pred)
-        # print(scmm$boot.sum[ , "mean"])
-        # matplot(L.pred, scmm$boot[, 1:5], type="l", add=TRUE)
-        ### END DEBUG ###
       } else {
         cat('\n Insufficient data \n')
       } # if (length...)
@@ -268,8 +182,6 @@ for (excl in c("Up","Down")) {
     } # if (sp %in% ...)
   } # for(sp)
   mtext('Size (mm)', side=1, outer=TRUE, at=c(0.5,0.5), cex=1.5)
-  mtext('Observed Catch per km', side=2, outer=TRUE, at=c(0.5,0.5), cex=1.5)
+  mtext('Observed Catch Per Km', side=2, outer=TRUE, at=c(0.5,0.5), cex=1.5)
   mtext('Estimated Catch Ratio', side=4, outer=TRUE, at=c(0.5,0.5), cex=1.5)
 } # for (excl)
-
-# Implementation of "double-bootstrap"
